bf8e27e63416e7ca17436459ba47c16ed3b1c338
Philip Kent <phil.kent@touchcast.co.nz>
2015-10-28 09:23:55 +1300

    chore(lib): initial commit

---
+++ chrome/register-node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xvfb-run --server-args=":99.0 -screen 0 2000x2000x16 -ac" \
+java -jar selenium-server-standalone-${VERSION}.jar \
+-role node \
+-hub http://$HUB_1_PORT_4444_TCP_ADDR:4444/grid/register \
+-browser browserName=chrome
+
+fluxbox -display ":99.0" &
+
+sleep 10
+
+x11vnc -forever -usepw -shared -rfbport 5900 -display ":99.0" &
